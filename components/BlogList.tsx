441ef6305bdffeadd52989a4ddbd777ec49061b2
Kristian Ivanov <kriskopk@abv.bg>
2023-04-07 16:31:00 +0300

    BlogList component done.

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,3 +1,5 @@
+import urlFor from '@/lib/urlFor';
+import Image from 'next/image';
 import React from 'react'
 
 type Props ={
@@ -6,7 +8,50 @@ type Props ={
 
 const BlogList = ({posts}:Props) => {
   return (
-    <div>BlogList</div>
+    <div>
+      <hr  className='border-[#F7AB0A] mb-10'/>
+
+      <div>
+        {posts.map(post=>(
+          <div key={post._id} className='flex flex-col group cursor-pointer'>
+            <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
+              <Image 
+              className='object-cover object-left'
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+              />
+              <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+                <div>
+                  <p className='font-bold'>{post.title}</p>
+                  <p>
+                    {new Date(post._createdAt).toLocaleDateString(
+                      "en-US",{
+                        day:"numeric",
+                        month:"long",
+                        year:"numeric",
+                      }
+                    )}
+                  </p>
+                </div>
+                <div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center'>
+                  {post.categories.map(category=>(
+                    <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
+                      <p>{category.title}</p>
+                    </div>
+                  ))}
+                </div>
+                   <div className='mt-5 flex-1'>
+                    <p className='underline text-lg font-bold'>{post.title}</p>
+                    <p className='text-gray-500'>{post.description}</p>
+                    </div> 
+
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
