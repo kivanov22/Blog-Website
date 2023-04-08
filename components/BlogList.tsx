32ff021b9b697c354e50e3c56146653078f72cb9
Kristian Ivanov <kriskopk@abv.bg>
2023-04-08 17:36:55 +0300

    BlogList component first part.

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,58 +1,62 @@
-import urlFor from '@/lib/urlFor';
-import Image from 'next/image';
-import React from 'react'
+import urlFor from "@/lib/urlFor";
+import Image from "next/image";
+import React from "react";
+import {ArrowRightIcon} from "@heroicons/react/24/solid";
 
-type Props ={
-    posts:Post[];
+type Props = {
+  posts: Post[];
 };
 
-const BlogList = ({posts}:Props) => {
+const BlogList = ({ posts }: Props) => {
   return (
     <div>
-      <hr  className='border-[#F7AB0A] mb-10'/>
+      <hr className="border-[#F7AB0A] mb-10" />
 
       <div>
-        {posts.map(post=>(
-          <div key={post._id} className='flex flex-col group cursor-pointer'>
-            <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
-              <Image 
-              className='object-cover object-left'
-              src={urlFor(post.mainImage).url()}
-              alt={post.author.name}
-              fill
+        {posts.map((post) => (
+          <div key={post._id} className="flex flex-col group cursor-pointer">
+            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+              <Image
+                className="object-cover object-left"
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
               />
-              <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+              <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
                 <div>
-                  <p className='font-bold'>{post.title}</p>
+                  <p className="font-bold">{post.title}</p>
                   <p>
-                    {new Date(post._createdAt).toLocaleDateString(
-                      "en-US",{
-                        day:"numeric",
-                        month:"long",
-                        year:"numeric",
-                      }
-                    )}
+                    {new Date(post._createdAt).toLocaleDateString("en-US", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
                   </p>
                 </div>
-                <div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center'>
-                  {post.categories.map(category=>(
-                    <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
+                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                  {post.categories.map((category) => (
+                    <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
                       <p>{category.title}</p>
                     </div>
                   ))}
                 </div>
-                   <div className='mt-5 flex-1'>
-                    <p className='underline text-lg font-bold'>{post.title}</p>
-                    <p className='text-gray-500'>{post.description}</p>
-                    </div> 
-
+                <div className="mt-5 flex-1">
+                  <p className="underline text-lg font-bold">{post.title}</p>
+                  <p className="line-clamp-2 text-gray-500">
+                    {post.description}
+                  </p>
+                </div>
+                <p className="mt-5 font-bold flex items-center group-hover:underline">
+                  Read Post
+                  <ArrowRightIcon className="ml-2 h-4 w-4" />
+                </p>
               </div>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogList
+export default BlogList;
